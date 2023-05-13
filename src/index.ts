6adfffacf27838d044be5d7aa04c30e3a455592d
Gianmarco Ciaffardini <marco@3sidedcube.com>
2023-05-13 20:44:48 +0200

    show env

--- src/index.ts
+++ src/index.ts
@@ -43,7 +43,7 @@ const soapWsdl = fs.readFileSync(
 
 app.get("/", (_: any, res: any) => {
   console.log("HIT HOMEPAGE");
-  res.send("online");
+  res.send(`online: ${process.env.SERVER_URL}`);
 });
 
 app.listen(port, () => {
